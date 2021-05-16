0ef8950fa5f4d9d790834cb9611b4dd6142f74f2
victormusa <victor.musacchio@globant.com>
2021-05-16 10:54:01 +0000

    SIIIII

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/project
-sudo sed -I "s/cicd-project:.*/cicd-project:$1/g" docker-compose.override.yml
+sed -I "s/cicd-project:.*/cicd-project:$1/g" docker-compose.override.yml
 docker-compose up -d
