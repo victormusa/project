54c3fd0617ab0e8dcb1537562b2cd9b893f3a78d
victormusa <victor.musacchio@globant.com>
2021-05-16 10:58:26 +0000

    por que en mayuscula?

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/project
-sed -I "s/cicd-project:.*/cicd-project:$1/g" docker-compose.override.yml
+sed -i "s/cicd-project:.*/cicd-project:$1/g" docker-compose.override.yml
 docker-compose up -d
