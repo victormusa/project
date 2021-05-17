de2dc86d0d5493280ac7c793cf0c8cdcd83c7755
victormusa <victormusa@gmail.com>
2021-05-17 18:07:28 +0000

    odio sudo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/project
-sed -i "s/victormusa/cicd-project:.*/victormusa/cicd-project:$1/g" docker-compose.override.yml
+sudo sed -i "s/victormusa/cicd-project:.*/victormusa/cicd-project:$1/g" docker-compose.override.yml
 sudo docker-compose up -d
