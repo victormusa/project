b52be6d825f21253aa4308bc1fbce8fb673419da
Victor Musacchio <victormusa@gmail.com>
2021-05-20 23:29:17 -0300

    joao me hizo romper todo

--- serverside/deploy.sh
+++ serverside/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/project
-sudo sed -i "s/cicd-project:.*/cicd-project:$1/g" docker-compose.override.yml
+sudo sed -i "s/$2-$3:.*/cicd-project:$1/g" docker-compose.override.yml
+sudo sed -i "s/victormusa/.*/victormusa/$2-$3/g" docker-compose.yaml
 sudo docker-compose up -d
